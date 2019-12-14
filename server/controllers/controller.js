aa3bd37600385faf08eb8a497f2062970dc0cf86
Sean Sweeney <seansweeney@Seans-MacBook-Pro.local>
2019-12-13 20:42:36 -0700

    backend

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -1,17 +1,43 @@
-let questions = [];
-let id = 0;
+let list = [{
+        id: 0,
+        statement: "HTML stands for HyperText Markup Language.",
+        answer: true
+    },
+    {
+        id: 1, 
+        statement: "React is a web application framework for Node.js.",
+        answer: false
+    },
+    {
+        id: 2,
+        statement: "Pigeons wear cowboy hats.",
+        answer: true
+    }];
+
+let id = 3;
 
 module.exports = {
-    create: (req, res) => {
-
+    getList: (req, res) => {
+        res.status(200).send(list)
     },
-    read: (req, res) => {
-
+    addStatement: (req, res) => {
+        const {statement, answer} = req.body
+        list.push({statement, answer, id})
+        id++
+        res.status(200).send(list)
     },
-    update: (req, res) => {
-
+    updateStatement: (req, res) => {
+        const updatedInfo = req.body;
+        const {id} = req.params;
+        const index = list.findIndex(e => e.id === +id);
+        const updatedObj = {...list[index], ...updatedInfo};
+        list[index] = updatedObj;
+        res.status(200).send(list)
     },
     delete: (req, res) => {
-        
+        const {id} = req.params;
+        const listIndex = list.findIndex(e => e.id === +id);
+        list.splice(listIndex, 1);
+        res.status(200).send(list);
     }
 }
--- server/server.js
+++ server/server.js
@@ -1,7 +1,15 @@
+require('dotenv').config()
 const express = require('express');
+      SERVER_PORT = 3002;
+      ctrl = require('./controllers/controller');
 
 const app = express();
+
 app.use(express.json());
-const SERVER_PORT = 3002;
+
+app.get('/api/list', ctrl.getList);
+app.post('/api/list', ctrl.addStatement);
+app.put('/api/list/:id', ctrl.updateStatement);
+app.delete('/api/list/:id', ctrl.delete);
 
 app.listen(SERVER_PORT, () => {console.log(`${SERVER_PORT} running.`)});

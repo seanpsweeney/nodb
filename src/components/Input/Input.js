ccc7d907f7917809bf5d139aef6001b3c12b1df2
Sean Sweeney <seansweeney@Seans-MacBook-Pro.local>
2019-12-19 19:45:41 -0700

    updated-3

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -5,32 +5,34 @@ class Input extends Component {
         super();
         this.state = {
             statement: '',
-        }
+        };
     }
 
     handleChange(e, key) {
         this.setState({
             [key]: e.target.value
-        })
+        });
     }
 
+    // * Used to clear input box once data is put in and submitted 
     handleSubmit() {
         const {statement} = this.state;
         this.props.addFn({statement});
         this.setState({
             statement: ''
-        })
-    }
+        });
+    };
 
     render() {
         const {statement} = this.state;
         return(
             <div className='input'>
                 <textarea onChange={e => this.handleChange(e, 'statement')} value={this.state.statement} placeholder='Enter your note here' type='text' />
+                <br/>
                 <button onClick={() => this.handleSubmit({statement})}>Save Note</button>
             </div>
         )
     }
-}
+};
 
 export default Input;
